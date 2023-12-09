b2849b5b8d370838a45512a877f8c11f75063553
bednarowicz <bednarowiczr@gmail.com>
2023-12-09 20:45:27 +0100

    adding scripts to package.json + example of usage of faker library

--- page-objects/formLayoutsPage.ts
+++ page-objects/formLayoutsPage.ts
@@ -1,20 +1,25 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 import { HelperBase } from "./helperBase";
 
 export class formLayoutsPage extends HelperBase {
     //private readonly page: Page
     readonly page: Page
+    private readonly usingTheGridForm: Locator
+    private readonly inLineForm: Locator
+
 
     constructor(page: Page){
         super(page)
+        this.usingTheGridForm = page.locator('nb-card', { hasText: "Using the Grid"})
+        this.inLineForm = page.locator('nb-card', {hasText: "Inline form"})
     }
 
     async submitUsingTheGridForm(email: string, password: string, optionText: string){
-        const usingTheGridForm = this.page.locator('nb-card', { hasText: "Using the Grid"})
-        await usingTheGridForm.getByRole('textbox', {name: 'Email'}).fill(email)
-        await usingTheGridForm.getByRole('textbox', {name: 'Password'}).fill(password)
-        await usingTheGridForm.getByRole('radio', {name: optionText}).check({force: true})
-        await usingTheGridForm.getByRole('button').click()
+        //const usingTheGridForm = this.page.locator('nb-card', { hasText: "Using the Grid"})
+        await this.usingTheGridForm.getByRole('textbox', {name: 'Email'}).fill(email)
+        await this.usingTheGridForm.getByRole('textbox', {name: 'Password'}).fill(password)
+        await this.usingTheGridForm.getByRole('radio', {name: optionText}).check({force: true})
+        await this.usingTheGridForm.getByRole('button').click()
     }
     /**
      * This method provides inline header with user data
@@ -23,12 +28,12 @@ export class formLayoutsPage extends HelperBase {
      * @param rememberMe 
      */
     async submitInLIneFormWithNameEmailAndCheckbox(name: string, email: string, rememberMe: boolean){
-        const inLineForm = this.page.locator('nb-card', {hasText: "Inline form"})
-        await inLineForm.getByRole('textbox', {name: 'Jane Doe'}).fill(name)
-        await inLineForm.getByRole('textbox', {name: 'Email'}).fill(email)
+        //const inLineForm = page.locator('nb-card', {hasText: "Inline form"})
+        await this.inLineForm.getByRole('textbox', {name: 'Jane Doe'}).fill(name)
+        await this.inLineForm.getByRole('textbox', {name: 'Email'}).fill(email)
         if(rememberMe){
-        await inLineForm.getByRole('checkbox').check({force: true})
+        await this.inLineForm.getByRole('checkbox').check({force: true})
         }
-        await inLineForm.getByRole('button').click()
+        await this.inLineForm.getByRole('button').click()
     }
 }
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -4,6 +4,7 @@ import { NavigationPage } from "../page-objects/navigationPage"
 import { formLayoutsPage } from "../page-objects/formLayoutsPage"
 import { DatepickerPage } from "../page-objects/datepickerPage"
 import { PageManager } from "../page-objects/pageManager"
+import {faker} from '@faker-js/faker'
 
 test.beforeEach('before each bethod', async ({ page }) => {
     await page.goto('http://localhost:4200/')
@@ -20,13 +21,15 @@ test('navigate to form page', async ({page}) => {
 
 test('parametrized methods', async({page}) => {
     const pm = new PageManager(page)
+    const randomFullName = faker.person.fullName()
+    const randomEmail = `${randomFullName}${faker.number.int(1000)}@test.com`
 
     await pm.navigateTo().formLayoutsPage()
     await pm.onFormLayoutsPage().submitUsingTheGridForm('test@com', "welcome", 'Option 2')
-    await pm.onFormLayoutsPage().submitInLIneFormWithNameEmailAndCheckbox('John smith', "test hohn", false)
+    await pm.onFormLayoutsPage().submitInLIneFormWithNameEmailAndCheckbox(randomFullName, randomEmail, false)
 
-    await pm.navigateTo().datePickerPage()
+    // await pm.navigateTo().datePickerPage()
 
-    await pm.onDatepickerPate().selectCommonDatePickerDateFromToday(30)
-    await pm.onDatepickerPate().selectDatepickerWithRangeFromTOday(6, 15)
+    // await pm.onDatepickerPate().selectCommonDatePickerDateFromToday(30)
+    // await pm.onDatepickerPate().selectDatepickerWithRangeFromTOday(6, 15)
 })
